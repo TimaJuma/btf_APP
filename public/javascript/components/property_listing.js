d55c3f6dce70fc3eae5c7cd62216fdab592dd75d
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-08-25 01:30:25 +0000

    Added a rotating post functionality

--- public/javascript/components/property_listing.js
+++ public/javascript/components/property_listing.js
@@ -1,26 +1,27 @@
 $(() => {
   window.propertyListing = {};
-  
+
   function createListing(item, isReservation) {
     return `
-    <article class="property-listing" data-aos="fade-in" data-aos-duration="1500">
-        <section class="property-listing__preview-image">
-          <img src="https://thumbs-prod.si-cdn.com/d4e3zqOM5KUq8m0m-AFVxuqa5ZM=/800x600/filters:no_upscale():focal(554x699:555x700)/https://public-media.si-cdn.com/filer/a4/04/a404c799-7118-459a-8de4-89e4a44b124f/img_1317.jpg" alt="house">
-        </section>
-        <section class="property-listing__details">
-          <h3 class="property-listing__title">${item.title}</h3>
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+    <img class="card__picture card__picture--1" src="https://www.catster.com/wp-content/uploads/2016/10/perfect-cat-photos-Penny-600x600.jpg" alt="cat">
+    <h4 class="card__heading">  <span class="card__heading-span">${item.title}</span> </h4>
           <ul class="property-listing__details">
             <li>Description: ${item.description}</li>
             <li>City: ${item.city}</li>
           </ul>
-          <footer class="property-listing__footer">
-            <div class="property-listing__price">$${item.price/100.0}CAD</div>
-          </footer>
-        </section>
-      </article>
-      `
+    </div>
+    <div class="flip-card-back">
+    <footer class="property-listing__footer">
+    <div class="property-listing__price">$${item.price / 100.0}CAD</div>
+  </footer>
+    </div>
+  </div>
+</div>
+      `;
   }
 
   window.propertyListing.createListing = createListing;
-
 });
