95a03aa3dda9105f10496eccff832c301217ede8
Brandon Lecky <dreamxinxcode@gmail.com>
2020-08-26 17:38:16 +0000

    navbar animation

--- public/javascript/nav.js
+++ public/javascript/nav.js
@@ -1,12 +1,13 @@
-$(function() {
-  const header = $(".navbar");
-
-  $(window).scroll(function() {
-      const scroll = $(window).scrollTop();
-      if (scroll >= 50) {
-          header.addClass("nav-dark");
-      } else {
-          header.removeClass("nav-dark");
-      }
-  });
+$(document).ready(function() {
+    $(window).scroll(function () {
+        const scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $('.navbar').addClass("nav-dark navbar-dark");
+            $('.navbar').removeClass("navbar-light");
+  
+        } else {
+            $('.navbar').removeClass("nav-dark navbar-dark");
+            $('.navbar').addClass("navbar-light");
+        }
+    });
 });
